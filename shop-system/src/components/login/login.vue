a426cd5802202a9bfcb58ab92f143e8ed59af7a2
TheDarkN1ght001 <1132962475@qq.com>
2019-06-20 17:20:26 +0800

    使用Element编写了login静态页面

--- shop-system/src/components/login/login.vue
+++ shop-system/src/components/login/login.vue
@@ -1,5 +1,20 @@
 <template>
-    <div class="login">我是login</div>
+    <div class="login">
+        <div class="content">
+            <el-form   label-position="top" :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                <h2>用户登录</h2>
+            <el-form-item label="用户名" prop="pass">
+                <el-input type="password" ></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="checkPass">
+                <el-input type="password" ></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" style="width:100%">登录</el-button>
+            </el-form-item>
+</el-form>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -10,8 +25,21 @@ export default {
 
 <style>
     .login {
+        position: relative;
         height: 100%;
         width: 100%;
         background-color: #324152;
+    }
+    .content {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+        width: 580px;
+        height: 460px;
+        background: #ffffff;
+        border-radius: 10px;
+        padding: 40px;
+        box-sizing: border-box;
     }
 </style>
