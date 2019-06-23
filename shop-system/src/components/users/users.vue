007096cfc2ab4396e2ee9a59d1482176b2ddf250
TheDarkN1ght001 <1132962475@qq.com>
2019-06-23 15:44:35 +0800

    完善了删除功能
    点击删除后弹框进行确定

--- shop-system/src/components/users/users.vue
+++ shop-system/src/components/users/users.vue
@@ -24,18 +24,24 @@
       <el-table-column prop="username" label="姓名" width="180"></el-table-column>
       <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
       <el-table-column prop="mobile" label="电话"></el-table-column>
-      <el-table-column prop="mg-state" label="用户状态" >
+      <el-table-column prop="mg-state" label="用户状态">
         <template slot-scope="scope">
           {{scope.row}}
           <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
         </template>
       </el-table-column>
-      <el-table-column label="操作" >
+      <el-table-column label="操作">
         <template slot-scope="scope">
-        <el-button type="primary" icon="el-icon-edit" size="mini" round></el-button>
-        {{scope.row.id}}
-        <el-button @click.prevent="del(scope.row.id)" type="danger" icon="el-icon-delete" size="mini" round></el-button>
-        <el-button type="success" icon="el-icon-check" size="mini" round></el-button>
+          <el-button type="primary" icon="el-icon-edit" size="mini" round></el-button>
+          {{scope.row.id}}
+          <el-button
+            @click.prevent="del(scope.row.id)"
+            type="danger"
+            icon="el-icon-delete"
+            size="mini"
+            round
+          ></el-button>
+          <el-button type="success" icon="el-icon-check" size="mini" round></el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -52,35 +58,35 @@
     <!-- 弹窗添加用户信息页面 -->
     <el-dialog title="添加用户" :visible.sync="Adddialog">
       <el-form>
-          {{addUserMsg}}
+        {{addUserMsg}}
         <el-form-item label="用户名" :label-width="formLabelWidth">
-          <el-input  v-model="addUserMsg.username" autocomplete="off"></el-input>
+          <el-input v-model="addUserMsg.username" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="密码" :label-width="formLabelWidth">
-          <el-input  v-model="addUserMsg.password" autocomplete="off"></el-input>
+          <el-input v-model="addUserMsg.password" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="邮箱" :label-width="formLabelWidth">
-          <el-input  v-model="addUserMsg.email" autocomplete="off"></el-input>
+          <el-input v-model="addUserMsg.email" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="电话" :label-width="formLabelWidth">
-          <el-input v-model="addUserMsg.mobile"  autocomplete="off"></el-input>
+          <el-input v-model="addUserMsg.mobile" autocomplete="off"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-    <el-button  @click.prevent="visnone">取 消</el-button>
-    <el-button type="primary"  @click.prevent="addUser">确 定</el-button>
-  </div>
+        <el-button @click.prevent="visnone">取 消</el-button>
+        <el-button type="primary" @click.prevent="addUser">确 定</el-button>
+      </div>
     </el-dialog>
   </el-card>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       tableData: [],
       // 查询关键字
-      query: '',
+      query: "",
       // 当前页
       pagenum: 1,
       // 页容量
@@ -92,117 +98,123 @@ export default {
       // 控制弹窗显影
       Adddialog: false,
       // 弹窗的宽度
-      formLabelWidth: '88px',
+      formLabelWidth: "88px",
       addUserMsg: {
-        username: '',
-        password: '',
-        email: '',
-        mobile: ''
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
       }
-    }
+    };
   },
   methods: {
-    getData () {
+    getData() {
       this.$http({
-        method: 'GET',
+        method: "GET",
         url: `http://localhost:8888/api/private/v1/users?query=${
           this.query
         }&pagenum=${this.pagenum}&pagesize=${this.pagesize}`,
         headers: {
-          Authorization: window.localStorage.getItem('token')
+          Authorization: window.localStorage.getItem("token")
         }
       }).then(res => {
         // console.log(res);
-        const { meta, data } = res.data
+        const { meta, data } = res.data;
         //判断data中users长度是否为0，为0，页数-1，并且重新获取数据
         if (meta.status === 200) {
-          if(data.users.length===0&&this.pagenum!==1){
-            this.pagenum--
-            this.getData()
+          if (data.users.length === 0 && this.pagenum !== 1) {
+            this.pagenum--;
+            this.getData();
             return;
           }
-          this.tableData = data.users
+          this.tableData = data.users;
           // 得到总条数
-          this.total = data.total
+          this.total = data.total;
         }
-      })
+      });
     },
-    changepage (currentPage) {
+    changepage(currentPage) {
       // console.log(currentPage);
-      this.pagenum = currentPage
-      this.getData()
+      this.pagenum = currentPage;
+      this.getData();
     },
-    sizeChange (pagesize) {
-      this.pagesize = pagesize
-      this.getData()
+    sizeChange(pagesize) {
+      this.pagesize = pagesize;
+      this.getData();
     },
     // 搜索用户信息
-    search () {
-      this.getData()
+    search() {
+      this.getData();
     },
     // 打开信息添加的弹窗
-    openAddPage () {
-      this.Adddialog = true
+    openAddPage() {
+      this.Adddialog = true;
     },
     // 弹窗的取消按钮（点击后影藏弹窗）
-    visnone () {
-      this.Adddialog = false
+    visnone() {
+      this.Adddialog = false;
     },
     // 添加用户信息
-    addUser () {
+    addUser() {
       this.$http({
-        method: 'POST',
-        url: 'http://localhost:8888/api/private/v1/users',
+        method: "POST",
+        url: "http://localhost:8888/api/private/v1/users",
         data: this.addUserMsg,
         headers: {
-          Authorization: window.localStorage.getItem('token')
+          Authorization: window.localStorage.getItem("token")
         }
       }).then(res => {
         if (res.data.meta.status === 201) {
           this.$message({
             message: res.data.meta.msg,
-            type: 'success'
-          })
-          this.getData()
+            type: "success"
+          });
+          this.getData();
         } else {
-          this.$message.error(res.data.meta.msg)
+          this.$message.error(res.data.meta.msg);
         }
         // 清空输入框数据
         for (var key in this.addUserMsg) {
-          this.addUserMsg[key] = ''
+          this.addUserMsg[key] = "";
         }
         // 关闭面板
-        this.Adddialog = false
-      })
+        this.Adddialog = false;
+      });
     },
     // 删除用户
-    del (id) {
+    del(id) {
       // console.log(id);
-      this.$http({
-        method: 'DELETE',
-        url: `http://localhost:8888/api/private/v1/users/${id}:id`,
-        headers: {
-          Authorization: window.localStorage.getItem('token')
-        }
-      }).then(res => {
-        if (res.data.meta.status == 200) {
-          this.getData()
-          // alert(res.data.meta.msg)
-          this.$message({
-            message: res.data.meta.msg,
-            type: 'success'
-          })
-        } else {
-          this.$message.error(res.data.meta.msg)
-        }
-      })
-    },
 
+      this.$confirm("你确定删除吗?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(() => {
+        this.$http({
+          method: "DELETE",
+          url: `http://localhost:8888/api/private/v1/users/${id}`,
+          headers: {
+            Authorization: window.localStorage.getItem("token")
+          }
+        }).then(res => {
+          if (res.data.meta.status == 200) {
+            this.getData();
+            // alert(res.data.meta.msg)
+            this.$message({
+              message: res.data.meta.msg,
+              type: "success"
+            });
+          } else {
+            this.$message.error(res.data.meta.msg);
+          }
+        });
+      });
+    }
   },
-  mounted () {
-    this.getData()
+  mounted() {
+    this.getData();
   }
-}
+};
 </script>
 
 <style>
